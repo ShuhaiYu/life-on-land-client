f46f65063034c82aabdadc1e960910231a2779b0
ShuhaiYu <1141071640@qq.com>
2024-04-08 21:21:44 +1000

    Update CSS styles and page heights

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,6 +1,6 @@
 const EducationPage = () => {
     return (
-        <div className="flex flex-col gap-10 items-center justify-center h-1/2">
+        <div className="flex flex-col gap-10 items-center justify-center h-full">
             <h1 className="text-4xl text-dark-grey font-bold m-10">Education Page</h1>
             <p className="text-2xl text-dark-grey font-bold">This page is not yet released</p>
             <p className="text-2xl text-dark-grey font-bold">Sorry for the inconvenience</p>
--- src/pages/Involved.jsx
+++ src/pages/Involved.jsx
@@ -1,7 +1,7 @@
 const InvolvedPage = () => {
     return (
 
-        <div className="flex flex-col gap-10 items-center justify-center h-1/2">
+        <div className="flex flex-col gap-10 items-center justify-center h-full">
             <h1 className="text-4xl text-dark-grey font-bold m-10">Involved Page</h1>
             <p className="text-2xl text-dark-grey font-bold">This page is not yet released</p>
             <p className="text-2xl text-dark-grey font-bold">Sorry for the inconvenience</p>
