645dba21afd18121794964ba15882d63bff6053c
ShuhaiYu <1141071640@qq.com>
2024-04-08 20:33:07 +1000

    Refactor Navbar component layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,15 @@ import { NavLink, Link, Outlet } from 'react-router-dom';
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar z-50">
-
-                <NavLink to="/" className="flex-none text-3xl w-10 text-white">WrenGuard</NavLink>
-                <div className="flex items-center justify-center gap-x-3 md:gap-12 mx-auto">
+            <nav className="navbar z-50 flex items-center justify-center w-full">
+                <NavLink to="/" className="text-3xl text-white mr-auto">WrenGuard</NavLink>
+                <div className="flex items-center justify-center md:gap-x-12 lg:gap-x-20 mx-auto" >
                     <NavLink to="/grasswren" className="link">Threatened Grasswren</NavLink>
                     <NavLink to="/risk" className="link">What Went Wrong</NavLink>
                     <NavLink to="/education" className="link">Becoming An Expert</NavLink>
                     <NavLink to="/involved" className="link">Get Involved</NavLink>
-
                 </div>
-
             </nav>
-            {/* Outlet is a special component that allows nested routing */}
             <Outlet />
         </>
 
