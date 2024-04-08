ceb803acf119ca847c69461e8f88c8f5e6782c39
ShuhaiYu <1141071640@qq.com>
2024-04-08 19:44:12 +1000

    Add new landing image and update text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LandingImg1 from '../imgs/home/bellbird-birding-tours-australian-birdwatching-tours-28.jpg'
 import LandingImg2 from '../imgs/home/grasswren-birdwatching-tour-bellbird-tours-1.jpg'
 import LandingImg3 from '../imgs/home/ebird2.jpeg'
+import LandingImg4 from '../imgs/home/grasswrens.png'
 import BirdIcon from '../imgs/bird.png'
 import Vector from '../imgs/Vector 1.png'
 import NavCardImg1 from '../imgs/home/nsw2.jpg'
@@ -12,7 +13,7 @@ import NavCard from '../components/NavCard'
 
 const HomePage = () => {
     return (
-        <>
+        <div className='flex flex-col'>
             {/* This is the landing imgs */}
             <div className="flex flex-col h-full">
                 <div className="flex flex-row">
@@ -28,7 +29,7 @@ const HomePage = () => {
                     <img src={LandingImg3} alt="Birdwatching" className="w-[40%] h-[40rem]" />
                 </div>
                 <div className='flex flex-row'>
-                    <img src={LandingImg2} alt="Grasswren" className="w-[40%]" />
+                    <img src={LandingImg4} alt="Grasswren" className="w-[40%]" />
                     <div className='flex flex-col justify-center items-center'>
                         <p className='text-2xl text-center font-bold w-2/3 my-5'>Three new species have recenlty become significantly threatened species across Australia. According to the NSW Threatened Species Scientific Committee (2021), the total population of Mukarrthippi Grasswren is very likely to be less than 20 mature individuals, with a continuing decline caused by ongoing threats from wildfire, habitat deterioration and fragmentation of the small populations.</p>
                         <img src={Vector} alt="Vector" className='w-auto h-auto' />
@@ -43,7 +44,7 @@ const HomePage = () => {
                 <NavCard title='how can i help?' img={NavCardImg2} link='/education' />
                 <NavCard title='how did it happen?' img={NavCardImg3} link='/risk' />
             </div>
-        </>
+        </div>
     )
 }
 
