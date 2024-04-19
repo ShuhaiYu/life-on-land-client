9fb6eb46538943181a5c773ee6b4ff5487c21867
ShuhaiYu <1141071640@qq.com>
2024-04-19 20:02:57 +1000

    Add scroll functionality to EducationPage for smooth navigation to topics

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import TopicSelectCard from "../components/TopicSelectCard";
 import { Link } from "react-router-dom";
 
@@ -16,6 +17,11 @@ import NavCardImg4 from '../imgs/home/getinvolved.jpeg'
 
 
 const EducationPage = () => {
+    const topicSelectRef = useRef(null); // Create a ref
+
+    const scrollToTopics = () => {
+        topicSelectRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
     return (
         <div className="flex flex-col bg-grey">
             {/* Part One */}
@@ -24,8 +30,8 @@ const EducationPage = () => {
                     <h1 className="text-[75px] font-bold">Become a WrenGuard</h1>
                     <p className="text-xl">Ready to test your knowledge and passion for threatened grasswrens? We've crafted four engaging topics to help you dive deep into their world and discover how we can all contribute to their conservation. Take the challenge and join the WrenGuard family today!</p>
                     <div className="flex flex-row gap-5 items-start justify-start w-full pt-10">
-                        <button className="btn-dark">Get Started</button>
-                        <button className="btn-light">Build A Habitat</button>
+                        <button className="btn-dark" onClick={scrollToTopics}>Get Started</button>
+                        <Link to="/habitat" className="btn-light">Build A Habitat</Link>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center w-1/2">
@@ -39,7 +45,7 @@ const EducationPage = () => {
                     conserve Australia's unique wildlife
                 </h1>
                 <p className="text-2xl text-center p-20 w-1/2">Select the topics below to start the challenge! Remember, your test result will disappear if you refresh the page!</p>
-                <div className="flex flex-row gap-5 items-start justify-start w-full p-20">
+                <div className="flex flex-row gap-5 items-start justify-start w-full p-20" ref={topicSelectRef}>
                     <TopicSelectCard topicName="Threatened Species" topicDescription="How do we determine if a species is threatened?" imgIcon={bird}/>
                     <TopicSelectCard topicName="Spot A Grasswren" topicDescription="How do we know if that is a grasswren?" imgIcon={telescope} />
                     <TopicSelectCard topicName="Where Do They Live" topicDescription="Reveal the secret of the grasswren's habitat!" imgIcon={nest} />
